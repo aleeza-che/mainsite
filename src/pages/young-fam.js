9155e62fb4b726b0880c3a73099521bfe2e83feb
aleeza-che <86495035+aleeza-che@users.noreply.github.com>
2025-01-11 20:05:08 -0500

    Update young-fam.js

--- src/pages/young-fam.js
+++ src/pages/young-fam.js
@@ -46,11 +46,11 @@ const SecondPage = () => (
 <Accordion>
             <Accordion.Item>
                   <Accordion.Header>
-                        Thursday, January 16, 2025 - Leviticus 27, Numbers 3-5 / Numbers 6
+                        Thursday, January 16, 2025 - Leviticus 27, Numbers 3-4 / Numbers 5
                   </Accordion.Header>
                   <Accordion.Body>
-                        <Link to="https://www.biblegateway.com/passage/?search=Leviticus%2027%2C%20Numbers%203-5&version=NLT;WEB;CJB" target="_blank">Numbers 3-5</Link><br/>
-                        <Link to="https://www.biblegateway.com/passage/?search=Numbers%206&version=NLT;WEB;CJB" target="_blank">Read Together Numbers 6</Link>
+                        <Link to="https://www.biblegateway.com/passage/?search=Leviticus%2027%2C%20Numbers%203-4&version=NLT;WEB;CJB" target="_blank">Numbers 3-4</Link><br/>
+                        <Link to="https://www.biblegateway.com/passage/?search=Numbers%205&version=NLT;WEB;CJB" target="_blank">Read Together Numbers 5</Link>
                   </Accordion.Body>
             </Accordion.Item>
       </Accordion>
