3f0c9f52fcf20580859d080e7a962d9d29a499b9
aleeza-che <86495035+aleeza-che@users.noreply.github.com>
2025-11-13 06:59:51 -0500

    Update young-fam.js

--- src/pages/young-fam.js
+++ src/pages/young-fam.js
@@ -47,6 +47,20 @@ const SecondPage = () => (
 <Accordion>
       <Accordion.Item>
             <Accordion.Header>
+                  Tuesday, November 18, 2025 - 1 Samuel 13-15 / 1 Samuel 16
+            </Accordion.Header>
+            <Accordion.Body>
+                  <Link to="https://www.biblegateway.com/passage/?search=1%20Samuel%2013-15&version=NLT;WEB;CJB" target="_blank">1 Samuel 13-15</Link><br/>
+                  <Link to="https://www.biblegateway.com/passage/?search=1%20Samuel%2016&version=NLT;WEB;CJB" target="_blank">1 Samuel 16</Link>
+            </Accordion.Body>
+      </Accordion.Item>
+</Accordion>
+
+
+
+<Accordion>
+      <Accordion.Item eventKey="0">
+            <Accordion.Header>
                   Tuesday, November 11, 2025 - 1 Samuel 9-11 / 1 Samuel 12
             </Accordion.Header>
             <Accordion.Body>
