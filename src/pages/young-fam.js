2dda47af8d67632ab2855b860cd504f65f0102f3
aleeza-che <86495035+aleeza-che@users.noreply.github.com>
2026-05-10 15:50:21 -0400

    update

--- src/pages/young-fam.js
+++ src/pages/young-fam.js
@@ -49,6 +49,25 @@ const SecondPage = () => (
 
 
             <Accordion.Header>
+                  Tuesday, May 12, 2026 - 2 Samuel 23, Psalm 108, 2 Samuel 24 / Psalms 60
+            </Accordion.Header>
+            <Accordion.Body>
+                  <Link to="https://www.biblegateway.com/passage/?search=2%20Samuel%2023%2C%20Psalm%20108%2C%202%20Samuel%2024&version=NLT;WEB;CJB" target="_blank">2 Samuel 20-22</Link><br/>
+
+                  <Link to="https://www.biblegateway.com/passage/?search=Psalm%2060&version=NLT;WEB;CJB" target="_blank">Psalms 60</Link>
+
+            </Accordion.Body>
+      </Accordion.Item>
+</Accordion>
+
+
+
+
+<Accordion>
+      <Accordion.Item eventKey="0">
+
+
+            <Accordion.Header>
                   Tuesday, May 5, 2026 - 2 Samuel 20-22 / Psalms 18
             </Accordion.Header>
             <Accordion.Body>
